ff32f46c984ade7d4552508b738e45dc63c839d5
Nethma Sandamini <59058898+nethma20@users.noreply.github.com>
2021-07-19 18:40:19 +0530

    Binary Search Tree

---
+++ src/binarysearchtree/BinaryTreeApp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarysearchtree;
+import java.util.Scanner;
+/**
+ *
+ * @author Senuda
+ */
+public class BinaryTreeApp {
+    public static void main(String args[]){
+        Scanner in=new Scanner(System.in);
+        System.out.println("1.Add a node");
+        System.out.println("2.Delete a node");
+        System.out.println("3.Show a min node");
+        System.out.println("4.Show max node");
+        System.out.println("5.Find a node");
+        System.out.println("6.Print all node");
+        System.out.println("7.Exit");
+        
+        System.out.println("Enter your ");
+    }
+}
